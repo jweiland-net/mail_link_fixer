402f57d3d5440c4caa941c2820326d12394610ab
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-03 14:48:31 +0200

    [TASK] Add copyright and license block to LegacyLinkRepository

--- Classes/Domain/Repository/LegacyLinkRepository.php
+++ Classes/Domain/Repository/LegacyLinkRepository.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the package jweiland/mail-link-fixer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace JWeiland\MailLinkFixer\Domain\Repository;
 
 use Doctrine\DBAL\Exception;
