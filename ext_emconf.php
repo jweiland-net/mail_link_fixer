c5afc71ebe517981556cffb3ef7066e48a77d2ba
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-16 09:23:48 +0200

    [TASK] Update TYPO3 version constraint in ext_emconf configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,18 +2,25 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the package jweiland/mail-link-fixer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Mail Link Fixer',
     'description' => 'CLI command to migrate legacy javascript:linkTo_UnCryptMailto() email hrefs in RTE fields to standard mailto: links.',
     'category' => 'backend',
-    'version' => '1.0.0',
-    'state' => 'stable',
     'author' => 'Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-14.99.99',
+            'typo3' => '13.4.0-14.3.99',
         ],
     ],
 ];
